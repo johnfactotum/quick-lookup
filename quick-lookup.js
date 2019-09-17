7498af97690174a74cde46c040e2972cf0b2327a
John Factotum <50942278+johnfactotum@users.noreply.github.com>
2019-09-17 12:31:04 +0800

    Misc cleanup

--- quick-lookup.js
+++ quick-lookup.js
@@ -20,6 +20,7 @@ const Webkit = imports.gi.WebKit2
 
 const baseURL = 'https://en.wiktionary.org/'
 const baseWikiRegExp = new RegExp('^https://en.wiktionary.org/wiki/')
+const apiURL = 'https://en.wiktionary.org/api/rest_v1/page/definition/'
 
 // see https://en.wiktionary.org/wiki/Wiktionary:Namespace
 const wikiNamespaces = [
@@ -46,8 +47,7 @@ const usurp = p => {
     p.parentNode.removeChild(p)
 }
 
-const pangoMarkupTags =
-    ['a', 'b', 'big', 'i', 's', 'sub', 'sup', 'small', 'tt', 'u']
+const pangoMarkupTags = ['a', 'b', 'big', 'i', 's', 'sub', 'sup', 'small', 'tt', 'u']
 const toPangoMarkup = (html, baseURL = '') => {
     const doc = new DOMParser().parseFromString(html, 'text/html')
     Array.from(doc.body.querySelectorAll('*')).forEach(el => {
@@ -66,37 +66,30 @@ const toPangoMarkup = (html, baseURL = '') => {
 }
 
 const baseURL = '${baseURL}'
-const wiktionary = (word, language = 'en') =>
-    fetch('https://en.wiktionary.org/api/rest_v1/page/definition/' + word)
-        .then(res => {
-            if (res.ok) return res
-            else throw new Error()
-        })
-        .then(res => res.json())
-        .then(json => {
-            const results = language.length === 2
-                ? json[language]
-                : Object.values(json).find(x => x[0].language === language)
-            
-            results.forEach(el => {
-                el.definitions.forEach(x => {
-                    x.definition = toPangoMarkup(x.definition, baseURL)
-                    if (x.examples)
-                        x.examples = x.examples.map(ex =>
-                            toPangoMarkup(ex, baseURL))
-                })
+const wiktionary = (word, language = 'en') => fetch('${apiURL}' + word)
+    .then(res => res.ok ? res.json() : Promise.reject(new Error()))
+    .then(json => {
+        const results = language.length === 2
+            ? json[language]
+            : Object.values(json).find(x => x[0].language === language)
+
+        results.forEach(el => {
+            el.definitions.forEach(x => {
+                x.definition = toPangoMarkup(x.definition, baseURL)
+                if (x.examples) x.examples = x.examples.map(ex =>
+                    toPangoMarkup(ex, baseURL))
             })
-            return { word: decodeURIComponent(word), results }
         })
-        .then(payload => dispatch({ type: 'lookup-results', payload }))
-        .catch(e => {
-            console.error(e)
-            word = decodeURI(word)
-            const lower = word.toLowerCase()
-            if (lower !== word)
-                dispatch({ type: 'lookup-again', payload: lower })
-            else dispatch({ type: 'lookup-error' })
-        })
+        return { word: decodeURIComponent(word), results }
+    })
+    .then(payload => dispatch({ type: 'lookup-results', payload }))
+    .catch(e => {
+        console.error(e)
+        word = decodeURI(word)
+        const lower = word.toLowerCase()
+        if (lower !== word) dispatch({ type: 'lookup-again', payload: lower })
+        else dispatch({ type: 'lookup-error' })
+    })
 
 dispatch({ type: 'can-lookup' })
 </script>
@@ -109,27 +102,19 @@ const lookup = (script, againScript) => new Promise((resolve, reject) => {
             allow_universal_access_from_file_urls: true
         })
     })
-    const scriptRun = script => webView.run_javascript(script, null, () => {})
+    const runScript = script => webView.run_javascript(script, null, () => {})
 
     webView.load_html(lookupHtml, null)
 
     const contentManager = webView.get_user_content_manager()
-    contentManager.connect('script-message-received::action', (_, message) => {
-        const data = message.get_js_value().to_string()
+    contentManager.connect('script-message-received::action', (_, jsResult) => {
+        const data = jsResult.get_js_value().to_string()
         const { type, payload } = JSON.parse(data)
         switch (type) {
-            case 'can-lookup':
-                scriptRun(script)
-                break
-            case 'lookup-again':
-                scriptRun(againScript(payload))
-                break
-            case 'lookup-results':
-                resolve(payload)
-                break
-            case 'lookup-error':
-                reject()
-                break
+            case 'can-lookup': runScript(script); break
+            case 'lookup-again': runScript(againScript(payload)); break
+            case 'lookup-results': resolve(payload); break
+            case 'lookup-error': reject(); break
         }
     })
     contentManager.register_script_message_handler('action')
@@ -176,12 +161,13 @@ class AppWindow {
         })
         const langEntry = new Gtk.Entry({
             placeholder_text: 'Language',
-            tooltip_text: 'Enter language name or ISO 639-1 code',
+            tooltip_text: 'Language name or ISO 639-1 code',
             width_chars: 10
         })
         langEntry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY,
             'preferences-desktop-locale-symbolic')
         langEntry.set_icon_sensitive(Gtk.EntryIconPosition.PRIMARY, false)
+
         const lookup = () => {
             if (this._currentPage) this._pushHistory(this._currentPage)
             this._lookup(this._queryEntry.text, this._langEntry.text)
@@ -189,19 +175,18 @@ class AppWindow {
         langEntry.connect('activate', lookup)
         queryEntry.connect('activate', lookup)
 
-        this._backButton = new Gtk.Button({
-            image: new Gtk.Image({ icon_name: 'go-previous-symbolic' }),
-            tooltip_text: 'Go back',
-            sensitive: false,
-            visible: true
-        })
-        this._backButton.connect('clicked', () => this._goBack())
-        headerBar.pack_start(this._backButton)
-
         const box = new Gtk.Box({ spacing: 6 })
         box.pack_start(langEntry, false, true, 0)
         box.pack_start(queryEntry, true, true, 0)
         headerBar.custom_title = box
+
+        this._backButton = new Gtk.Button({
+            image: new Gtk.Image({ icon_name: 'go-previous-symbolic' }),
+            tooltip_text: 'Go back',
+            sensitive: false
+        })
+        this._backButton.connect('clicked', () => this._goBack())
+        headerBar.pack_start(this._backButton)
 
         const content = new Gtk.Box({
             valign: Gtk.Align.CENTER,
@@ -255,7 +240,7 @@ class AppWindow {
         this._content = box
         this._window.add(this._content)
         this._content.show_all()
-    
+
         const handleLink = (_, uri) => {
             const internalLink = uri.split(baseWikiRegExp)[1]
             if (internalLink && wikiNamespaces.every(namespace =>
@@ -271,56 +256,61 @@ class AppWindow {
         }
 
         wiktionary(query, language).then(({ word, results }) => {
+            const displayWord = word.replace(/_/g, ' ')
+            const displayLanguage = results[0].language
+            const linkLanguage = displayLanguage.replace(/ /g, '_')
+
             const grid = new Gtk.Grid({
                 column_spacing: 6, row_spacing: 6,
                 border_width: 18,
                 valign: Gtk.Align.START
             })
             let row = 0
-            results.forEach(({ partOfSpeech, definitions, language }, i) => {
-                if (i === 0) {
-                    const langLabel = new Gtk.Label({
-                        label: `<small>${language}</small>`,
-                        xalign: 0,
-                        use_markup: true
-                    })
-                    langLabel.get_style_context().add_class('dim-label')
-                    grid.attach(langLabel, 1, row, 1, 1)
-                    row++
-                    
-                    const title = new Gtk.Label({
-                        label: '<span size="x-large" weight="bold">'
-                            + GLib.markup_escape_text(word.replace(/_/g, ' '), -1)
-                            + '</span>',
-                        xalign: 0,
-                        use_markup: true,
-                        selectable: true
-                    })
-                    grid.attach(title, 1, row, 1, 1)
-                    title.set_line_wrap(true)
-                    row++
-                } else {
+
+            const langLabel = new Gtk.Label({
+                label: `<small>${displayLanguage}</small>`,
+                xalign: 0,
+                use_markup: true
+            })
+            langLabel.get_style_context().add_class('dim-label')
+            grid.attach(langLabel, 1, row, 1, 1)
+            row++
+
+            const title = new Gtk.Label({
+                label: '<span size="x-large" weight="bold">'
+                    + GLib.markup_escape_text(displayWord, -1)
+                    + '</span>',
+                xalign: 0,
+                use_markup: true,
+                selectable: true
+            })
+            grid.attach(title, 1, row, 1, 1)
+            title.set_line_wrap(true)
+            row++
+
+            results.forEach(({ partOfSpeech, definitions }, i) => {
+                if (i > 0) {
                     grid.attach(new Gtk.Label(), 1, row, 1, 1)
                     row++
                 }
-                
-                const posBox = new Gtk.Box({ spacing: 6 })
-                const posLabel = new Gtk.Label({
+
+                const partOfSpeechBox = new Gtk.Box({ spacing: 6 })
+                const partOfSpeechLabel = new Gtk.Label({
                     label: `<i>${partOfSpeech}</i>`,
                     xalign: 0,
                     use_markup: true
                 })
-                posLabel.get_style_context().add_class('dim-label')
-                posBox.pack_start(posLabel, false, true, 0)
-                posBox.pack_start(new Gtk.Separator({
+                partOfSpeechLabel.get_style_context().add_class('dim-label')
+                partOfSpeechBox.pack_start(partOfSpeechLabel, false, true, 0)
+                partOfSpeechBox.pack_start(new Gtk.Separator({
                     valign: Gtk.Align.CENTER
                 }), true, true, 0)
                 grid.attach(new Gtk.Separator({
                     valign: Gtk.Align.CENTER
                 }), 0, row, 1, 1)
-                grid.attach(posBox, 1, row, 1, 1)
+                grid.attach(partOfSpeechBox, 1, row, 1, 1)
                 row++
-                
+
                 definitions.forEach(({ definition, examples }, i) => {
                     const label = new Gtk.Label({
                         label: i + 1 + '.',
@@ -343,7 +333,7 @@ class AppWindow {
                     value.connect('activate-link', handleLink)
                     grid.attach(value, 1, row, 1, 1)
                     row++
-                    
+
                     if (examples) {
                         const exampleBox = new Gtk.Box({
                             orientation: Gtk.Orientation.VERTICAL,
@@ -379,7 +369,7 @@ class AppWindow {
             const sourceLabel = new Gtk.Label({
                 label: `<small>Source: <a href="${baseURL}wiki/${
                     GLib.markup_escape_text(word, -1)
-                }#${results[0].language}">Wiktionary</a></small>`,
+                }#${linkLanguage}">Wiktionary</a></small>`,
                 xalign: 1,
                 use_markup: true
             })
